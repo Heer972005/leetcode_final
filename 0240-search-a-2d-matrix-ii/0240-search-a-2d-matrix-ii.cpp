12674f0399ab9c96515242594e8f8941b1d5346f
Heer972005 <mehtaheer.972@gmail.com>
2026-08-08 20:40:10 +0530

    Time: 121 ms (8.38%), Space: 18.6 MB (69.2%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,12 +1,63 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(target==matrix[i][j])
-                    return true;
-            }
+    bool helper(vector<vector<int>>& matrix,
+                int sr, int sc,
+                int er, int ec,
+                int target) {
+
+        // Base Case
+        if (sr > er || sc > ec)
+            return false;
+
+        // Find middle cell
+        int midr = (sr + er) / 2;
+        int midc = (sc + ec) / 2;
+
+        int value = matrix[midr][midc];
+
+        // Target found
+        if (value == target)
+            return true;
+
+        // Middle value is smaller than target
+        if (value < target) {
+
+            return helper(matrix,
+                          midr + 1, sc,
+                          er, ec,
+                          target)
+
+                ||
+
+                   helper(matrix,
+                          sr, midc + 1,
+                          midr, ec,
+                          target);
         }
-        return false;
+
+        // Middle value is greater than target
+        return helper(matrix,
+                      sr, sc,
+                      er, midc - 1,
+                      target)
+
+            ||
+
+               helper(matrix,
+                      sr, midc,
+                      midr - 1, ec,
+                      target);
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        if (matrix.empty() || matrix[0].empty())
+            return false;
+
+        return helper(matrix,
+                      0, 0,
+                      matrix.size() - 1,
+                      matrix[0].size() - 1,
+                      target);
     }
 };
